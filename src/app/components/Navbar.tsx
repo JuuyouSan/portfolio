95a2443f3b3eef96f7ae62f2fd188930b67c8e17
Juuyou <justintruong222@gmail.com>
2025-12-14 01:18:10 -0600

    navbar css cleanup

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import "./Navbar.css";
 
 export default function Navbar() {
   const pathname = usePathname();
@@ -13,26 +14,23 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="flex justify-center items-center py-6 px-4">
-      <div className="flex space-x-8">
+    <nav className="navbar-container">
+      <div className="navbar-links-display">
         {navItems.map((item) => {
           const isActive = pathname === item.href;
           return (
             <Link
               key={item.name}
               href={item.href}
-              className={`relative text-lg transition-colors duration-200`}
+              className={"navbar-links"}
             >
               {item.name}
               <span
-                className={`absolute -bottom-1 left-1/2 h-0.5 transform -translate-x-1/2 transition-all duration-300 ${
+                className={`navbar-link-underline ${
                   isActive 
-                    ? 'w-full' 
-                    : 'w-0 hover:w-full'
+                    ? "active" 
+                    : ""
                 }`}
-                style={{
-                  backgroundColor: 'var(--foreground)'
-                }}
               />
             </Link>
           );
